19972027cbf3cb9a604efac038c5e02ebdcc1bba
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-12-24 23:00:34 +0330

    update: test Route getter methods

--- tests/Route/Route.test.js
+++ tests/Route/Route.test.js
@@ -14,4 +14,26 @@ describe("test routes", function(){
 
         expect(route).toBeInstanceOf(Route);
     });
+
+    it("Route Getter", function(){
+        let data = {
+            method: MethodEnum.delete,
+            route: '/api/getter-test',
+            middleware: ['web'],
+            action: () => { return 'getter-action' }
+        };
+
+        let route = new Route(data.method, data.route, data.middleware, data.action);
+
+        expect(route.getMethod()).toBe(data.method);
+        expect(route.getRoute()).toBe(data.route);
+        expect(route.getMiddleware()).toBe(data.middleware);
+        expect(route.getAction()).toBe(data.action);
+
+        // test if middleware it's just empty array
+        data.middleware = [];
+        route = new Route(data.method, data.route, data.middleware, data.action);
+
+        expect(route.getMiddleware()).toBe(data.middleware);
+    });
 });
