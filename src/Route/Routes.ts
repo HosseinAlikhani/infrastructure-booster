e709ab1055d66783b4144645df8abc6ceca8feb8
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-12-27 23:49:15 +0330

    update: add comments + add getRoutes + group methods

--- src/Route/Routes.ts
+++ src/Route/Routes.ts
@@ -5,8 +5,33 @@ export default class Routes
 {
     private routes: Array<GroupRoute|Route> = [];
 
-    public setRoute(route: GroupRoute|Route){
+    /**
+     * register route to routes property
+     * @param route 
+     */
+    public route(route: GroupRoute|Route){
         this.routes.push(route);
     }
 
+    /**
+     * get routes
+     * @returns 
+     */
+    public getRoutes(): Array<GroupRoute|Route>
+    {
+        return this.routes;
+    }
+
+    /**
+     * register group route
+     * @param prefix 
+     * @param middleware 
+     * @param callback 
+     */
+    public group(prefix: String, middleware: any, callback: Function): void
+    {
+        this.route(
+            new GroupRoute(prefix, middleware, callback)
+        );
+    }
 }
