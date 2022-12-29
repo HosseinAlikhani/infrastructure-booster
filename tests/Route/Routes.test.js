1d0b85029baa1e37b1da8fc8df486d32ee30f2a3
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-12-29 15:49:44 +0330

    update: update check group method in routesTest

--- tests/Route/Routes.test.js
+++ tests/Route/Routes.test.js
@@ -22,6 +22,18 @@ describe('test Routes', () => {
     it('check group/getRoute methods', () => {
         let routes = new Routes(),
             routeFakes = [];
-        
+
+        let group1 = routeFaker.group();
+        routeFakes.push( routes.group(group1.prefix, group1.middleware, group1.callback) );
+
+        for(let i = 1; i <= 4; i++){
+            routeFakes.push( routes.route(routeFaker.route()) );
+        }
+
+        let group3  = routeFaker.group();
+        routeFakes.push( routes.group(group3.prefix, group3.middleware, group3.callback) );
+
+        expect( routes.getRoutes().length ).toBe(6);
+        expect( routes.getRoutes() ).toEqual( routeFakes );
     });
 });
