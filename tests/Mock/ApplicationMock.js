3821d4e3c7ec72d1fb6b2d9253a03c006e531042
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-12-30 21:29:17 +0330

    update: update return of application method in ApplicationMock

--- tests/Mock/ApplicationMock.js
+++ tests/Mock/ApplicationMock.js
@@ -1,11 +1,18 @@
+let app = {
+    listen
+};
+
 function Application(){
-    // application mock
+    return app;
 }
 
 function Router(){
     // application router mock
 }
 
+function listen(port, callback){
+    // run application mock
+}
 
 exports = module.exports = Application;
 exports.Router = Router;
