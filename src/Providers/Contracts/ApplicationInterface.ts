560643b172303d534d051f00e6f13fbe4140a67d
hossein.alikhani <hossein.alikhani1374@gmail.com>
2022-12-30 16:16:07 +0330

    update: remove _router / request / response / Router  from applicationInterface

--- src/Providers/Contracts/ApplicationInterface.ts
+++ src/Providers/Contracts/ApplicationInterface.ts
@@ -1,13 +1,9 @@
 export interface ApplicationInterface
 {
-    _router;
-    request;
-    response;
     get(name, middleware, action);
     post(name, middleware, action);
     patch(name, middleware, action);
     delete(name, middleware, action);
-    Router();
     use(...arg);
     listen(port, callback);
 }
