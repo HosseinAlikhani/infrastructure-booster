cf2917e6b6c54be76aa5c13d0439ce89d3ac76a2
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-01 21:44:25 +0330

    update: check method in test get method

--- tests/Route/Routes.test.js
+++ tests/Route/Routes.test.js
@@ -37,13 +37,14 @@ describe('test Routes', () => {
 
     it('check get method', () => {
         let routes = new Routes(),
-            fakeRoute = routeFaker.route();
+            fakeRoute = routeFaker.route('get');
         routes.get( fakeRoute.getRoute(), fakeRoute.getMiddleware(), fakeRoute.getAction() );
 
         expect(routes.getRoutes().length).toBe(1);
 
         let route = routes.getRoutes()[0];
 
+        expect( route.getMethod() ).toBe( fakeRoute.getMethod() );
         expect( route.getRoute() ).toBe( fakeRoute.getRoute() );
         expect( route.getMiddleware() ).toBe( fakeRoute.getMiddleware() );
         expect( route.getAction() ).toBe( fakeRoute.getAction() );
