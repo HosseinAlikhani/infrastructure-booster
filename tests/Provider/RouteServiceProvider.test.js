ff4fdf5bf0bde793f5f1ff0f9b529577a08d32fd
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-01 22:58:13 +0330

    [WIP] add RouteServiceProvider

---
+++ tests/Provider/RouteServiceProvider.test.js
@@ -0,0 +1,19 @@
+import Routes from "./../../src/Route/Routes";
+import RouteFaker from "./../Faker/RouteFaker";
+
+const routeFaker = new RouteFaker();
+describe("test RouteServiceProvider", () => {
+
+    it("test registerRoutes method", () => {
+        let routes = new Routes(),
+            fakeRoute = [
+                routeFaker.route('get'),
+                routeFaker.route('post'),
+                routeFaker.group(),
+                routeFaker.route('post'),
+                routeFaker.route('post'),
+                routeFaker.route('delete'),
+            ];
+    });
+
+});
