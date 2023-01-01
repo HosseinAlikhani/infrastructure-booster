839dc0888dbd0c435d2b63fdee21ab8e6286e9ed
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-01 21:45:09 +0330

    update: check post method

--- tests/Route/Routes.test.js
+++ tests/Route/Routes.test.js
@@ -49,4 +49,19 @@ describe('test Routes', () => {
         expect( route.getMiddleware() ).toBe( fakeRoute.getMiddleware() );
         expect( route.getAction() ).toBe( fakeRoute.getAction() );
     });
+
+    it('check post method', () => {
+        let routes = new Routes(),
+        fakeRoute = routeFaker.route('post');
+        routes.post( fakeRoute.getRoute(), fakeRoute.getMiddleware(), fakeRoute.getAction() );
+
+        expect(routes.getRoutes().length).toBe(1);
+
+        let route = routes.getRoutes()[0];
+
+        expect( route.getMethod() ).toBe( fakeRoute.getMethod() );
+        expect( route.getRoute() ).toBe( fakeRoute.getRoute() );
+        expect( route.getMiddleware() ).toBe( fakeRoute.getMiddleware() );
+        expect( route.getAction() ).toBe( fakeRoute.getAction() );
+    });
 });
