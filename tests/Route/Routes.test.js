0827426d45df21e15882fa5d48caa27f7e450039
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-01 21:46:37 +0330

    update: check delete method

--- tests/Route/Routes.test.js
+++ tests/Route/Routes.test.js
@@ -79,4 +79,19 @@ describe('test Routes', () => {
         expect( route.getMiddleware() ).toBe( fakeRoute.getMiddleware() );
         expect( route.getAction() ).toBe( fakeRoute.getAction() );
     });
+
+    it('check delete method', () => {
+        let routes = new Routes(),
+        fakeRoute = routeFaker.route('delete');
+        routes.delete( fakeRoute.getRoute(), fakeRoute.getMiddleware(), fakeRoute.getAction() );
+
+        expect(routes.getRoutes().length).toBe(1);
+
+        let route = routes.getRoutes()[0];
+
+        expect( route.getMethod() ).toBe( fakeRoute.getMethod() );
+        expect( route.getRoute() ).toBe( fakeRoute.getRoute() );
+        expect( route.getMiddleware() ).toBe( fakeRoute.getMiddleware() );
+        expect( route.getAction() ).toBe( fakeRoute.getAction() );
+    });
 });
