a6f75e3f237f360788b5436ee436520bf100976f
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-03 23:50:32 +0330

    WIP add some method for route

--- tests/Mock/ApplicationMock.js
+++ tests/Mock/ApplicationMock.js
@@ -10,9 +10,42 @@ function Router(){
     // application router mock
 }
 
+
+// inner methods and property
+var routes = [];
+
 function listen(port, callback){
     // run application mock
 }
+
+function get(route, middleware, action){
+    routes.push({
+        method: 'get',
+        route,
+        middleware,
+        action
+    });
+}
+
+function post(route, middleware, action){
+    routes.push({
+        method: 'post',
+        route,
+        middleware,
+        action
+    });
+}
+
+function patch(route, middleware, action){
+    routes.push({
+        method: 'patch',
+        route,
+        middleware,
+        action
+    });
+}
+
+
 
 exports = module.exports = Application;
 exports.Router = Router;
