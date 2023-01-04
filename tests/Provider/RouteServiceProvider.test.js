7765bfb1556644e23aa659caa09130268d6cbfea
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-04 23:55:21 +0330

    WIP update route service provider test

--- tests/Provider/RouteServiceProvider.test.js
+++ tests/Provider/RouteServiceProvider.test.js
@@ -1,5 +1,9 @@
 import Routes from "./../../src/Route/Routes";
 import RouteFaker from "./../Faker/RouteFaker";
+import RouteServiceProvider from "./../../src/Providers/RouteServiceProvider";
+
+const application = require('./../Mock/ApplicationMock');
+
 
 const routeFaker = new RouteFaker();
 describe("test RouteServiceProvider", () => {
@@ -9,15 +13,17 @@ describe("test RouteServiceProvider", () => {
             fakeRoute = [
                 routeFaker.route('get'),
                 routeFaker.route('post'),
-                routeFaker.group(),
+                // routeFaker.group(),
                 routeFaker.route('post'),
                 routeFaker.route('post'),
-                routeFaker.route('delete'),
             ];
 
         fakeRoute.forEach( (route) => {
             routes.route(route);
         });
+
+        let routeServiceProvider = new RouteServiceProvider(application);
+        routeServiceProvider.registerRoutes(routes);
 
     });
 
