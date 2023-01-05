66bcb89c67503406fffd19b0dfb9664723e6a990
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-05 20:36:00 +0330

    update: generate dynamic httpMethods in applicationMock

--- tests/Mock/ApplicationMock.js
+++ tests/Mock/ApplicationMock.js
@@ -3,9 +3,6 @@ let _routes = [];
 let app = {
     _routes,
     listen,
-    get,
-    post,
-    patch
 };
 
 function Application(){
@@ -23,33 +20,21 @@ function listen(port, callback){
     // run application mock
 }
 
-function get(route, middleware, action){
-    _routes.push({
-        method: 'get',
-        route,
-        middleware,
-        action
-    });
-}
 
-function post(route, middleware, action){
-    _routes.push({
-        method: 'post',
-        route,
-        middleware,
-        action
-    });
-}
+let routeMethods = {};
 
-function patch(route, middleware, action){
-    _routes.push({
-        method: 'patch',
-        route,
-        middleware,
-        action
-    });
-}
+['get', 'post', 'patch', 'delete'].forEach( (httpVerb) =>{
+    routeMethods = { ...routeMethods, [httpVerb]: (route, middleware, action) => {
+        _routes.push({
+            method: httpVerb,
+            route,
+            middleware,
+            action
+        });
+    }};
+});
 
+app = {...app, ...routeMethods};
 
 exports = module.exports = Application;
 exports.Router = Router;
