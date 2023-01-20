2c558bd97e5bd4b5ebb81c9ae7e0a46f3d2aeb95
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-20 16:21:30 +0330

    update: add getApplication method

--- src/Providers/Contracts/ServiceProviderInterface.ts
+++ src/Providers/Contracts/ServiceProviderInterface.ts
@@ -1,6 +1,14 @@
+import { ApplicationInterface } from "./ApplicationInterface";
+
 export interface ServiceProviderInterface
 {
     getApplicationSource();
+
+    /**
+     * return application instance
+     * @returns ApplicationInterface
+     */
+    getApplication(): ApplicationInterface
     
     listen(port: null|number);
 }
