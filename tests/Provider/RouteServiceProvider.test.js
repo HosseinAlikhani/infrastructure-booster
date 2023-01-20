40364c87bebda9ac7b1f595f326c16793f6a8f84
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-20 16:26:22 +0330

    update: update RouteServicePRovider test

--- tests/Provider/RouteServiceProvider.test.js
+++ tests/Provider/RouteServiceProvider.test.js
@@ -1,12 +1,12 @@
 import Routes from "./../../src/Route/Routes";
 import RouteFaker from "./../Faker/RouteFaker";
 import RouteServiceProvider from "./../../src/Providers/RouteServiceProvider";
-
+import ServiceProvider from "infrastructure-booster/src/Providers/ServiceProvider";
 const application = require('./../Mock/ApplicationMock');
-
 
 const routeFaker = new RouteFaker();
 describe("test RouteServiceProvider", () => {
+    ServiceProvider.init(application);
 
     it("test registerRoutes method", () => {
         let routes = new Routes(),
