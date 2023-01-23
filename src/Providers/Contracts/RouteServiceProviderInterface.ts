ff9abf1990c28f0ac889354cd2be7c8cb23d0ac2
hossein.alikhani <hossein.alikhani1374@gmail.com>
2023-01-23 22:36:47 +0330

    update: add registerMiddlewares in route service provider interface

--- src/Providers/Contracts/RouteServiceProviderInterface.ts
+++ src/Providers/Contracts/RouteServiceProviderInterface.ts
@@ -3,4 +3,11 @@ import { RoutesInterface } from "../../Route/RouteInterface";
 export interface RouteServiceProviderInterface
 {
     registerRoutes(applicationRoutes: RoutesInterface): boolean;
+    
+    /**
+    * register middleware to route service provider
+    * @param name
+    * @param middleware 
+    */
+    registerMiddlewares(name: string, middleware: Function): void;
 }
